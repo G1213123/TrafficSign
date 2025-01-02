185ca0e136833e552dc04737019c132bbdef2736
Andrew Ng <ngwingfung2136@yahoo.com.hk>
2025-01-03 00:00:07 +0800

    0.5

--- js/draw.js
+++ js/draw.js
@@ -17,126 +17,6 @@ var TextBlock = fabric.util.createClass(fabric.Textbox, {
   }
 });
 
-class GlyphGroup extends fabric.Group{
-  constructor(polygons, options){
-    super(polygons, options);
-    this.subObjects = []
-    polygons.forEach(p =>{
-      p.vertex.forEach(v => {
-        // Draw a halftone circle 
-      const circle = new fabric.Circle({
-        left: v.x,
-        top: v.y,
-        radius: 15,
-        fill: 'rgba(180, 180, 180, 0.5)',
-        selectable: false,
-        originX: 'center',
-        originY: 'center',
-      });
-      this.subObjects.push(circle);
-
-      // Add a text label 
-      const text = new fabric.Text(v.label, {
-        left: v.x,
-        top: v.y - 30,
-        fontSize: 20,
-        fill: 'red',
-        selectable: false,
-        originX: 'center',
-        originY: 'center',
-      });
-      this.subObjects.push(text);
-      })
-    })
-
-  }
-}
-
-class GlyphPolygon extends fabric.Polygon {
-  constructor(vertex, options) {
-    super(vertex.map(p => ({ x: p.x, y: p.y })), options);
-    vertex.forEach((p) => {
-      p.x = p.x + options.left;
-      p.y = p.y + options.top
-    });
-    this.vertex = vertex // Add a list inside the object
-    this.on('moving', this.onMoving.bind(this)); // Listen for modifications
-    this.on('modified', this.onMoving.bind(this)); // Listen for modifications
-    this.on('selected', this.onSelected.bind(this)); // Show vertex hints when selected
-    this.on('deselected', this.onDeselected.bind(this));
-  }
-
-  }
-
-
-  // Method to get the corners
-  getCorners() {
-    const { left, top, width, height, angle } = this.getBoundingRect();
-    const rad = fabric.util.degreesToRadians(angle);
-
-    // Calculate corners based on bounding box and rotation
-    const corners = [
-      { x: left, y: top }, // Top-left
-      { x: left + width, y: top }, // Top-right
-      { x: left + width, y: top + height }, // Bottom-right
-      { x: left, y: top + height } // Bottom-left
-    ];
-
-    return corners.map(corner => {
-      return {
-        x: (corner.x - left) * Math.cos(rad) - (corner.y - top) * Math.sin(rad) + left,
-        y: (corner.x - left) * Math.sin(rad) + (corner.y - top) * Math.cos(rad) + top
-      };
-    });
-  }
-
-  // Method to update vertices
-  onMoving() {
-    const transformedPoints = this.calculateTransformedPoints(this.points, {
-      x: this.left,
-      y: this.top,
-      angle: this.angle
-    });
-
-    // Update customList with new coordinates 
-    transformedPoints.forEach((point, index) => {
-      this.vertex[index].x = point.x;
-      this.vertex[index].y = point.y;
-    });
-
-    canvas.renderAll();
-
-  }
-
-  // Method to calculate transformed points
-  calculateTransformedPoints(points, options) {
-    const { x, y, angle } = options;
-    const radians = fabric.util.degreesToRadians(angle);
-
-    return points.map(point => {
-      const translatedX = point.x;
-      const translatedY = point.y;
-      const rotatedX = translatedX * Math.cos(radians) - translatedY * Math.sin(radians);
-      const rotatedY = translatedX * Math.sin(radians) + translatedY * Math.cos(radians);
-      return { x: rotatedX + x, y: rotatedY + y };
-    });
-  }
-
-  onSelected() {
-    if (this.group) {
-      this.group.set('opacity', 1);
-      canvas.renderAll();
-    }
-  }
-
-  onDeselected() {
-    if (this.group) { 
-      this.group.set('opacity', 1); 
-      canvas.renderAll(); }
-  }
-
-}
-
 
 function AddPlate() {
   var rect = new fabric.Rect({
@@ -191,6 +71,146 @@ function calculateTransformedPoints(points, options) {
   });
 }
 
+class GlyphPolygon extends fabric.Polygon {
+  constructor(vertex, options) {
+    super(vertex.map(p => ({ x: p.x, y: p.y })), options);
+    vertex.forEach((p) => {
+      p.x = p.x + options.left;
+      p.y = p.y + options.top
+    });
+    this.vertex = vertex // Add a list inside the object
+    this.on('moving', this.onMoving.bind(this)); // Listen for modifications
+    this.on('modified', this.onMoving.bind(this)); // Listen for modifications
+  }
+
+  // Method to get the corners
+  getCorners() {
+    const { left, top, width, height, angle } = this.getBoundingRect();
+    const rad = fabric.util.degreesToRadians(angle);
+
+    // Calculate corners based on bounding box and rotation
+    const corners = [
+      { x: left, y: top }, // Top-left
+      { x: left + width, y: top }, // Top-right
+      { x: left + width, y: top + height }, // Bottom-right
+      { x: left, y: top + height } // Bottom-left
+    ];
+
+    return corners.map(corner => {
+      return {
+        x: (corner.x - left) * Math.cos(rad) - (corner.y - top) * Math.sin(rad) + left,
+        y: (corner.x - left) * Math.sin(rad) + (corner.y - top) * Math.cos(rad) + top
+      };
+    });
+  }
+
+  // Method to update vertices
+  onMoving() {
+    const transformedPoints = this.calculateTransformedPoints(this.points, {
+      x: this.left,
+      y: this.top,
+      angle: this.angle
+    });
+
+    // Update customList with new coordinates 
+    transformedPoints.forEach((point, index) => {
+      this.vertex[index].x = point.x;
+      this.vertex[index].y = point.y;
+    });
+
+    canvas.renderAll();
+
+  }
+
+  // Method to calculate transformed points
+  calculateTransformedPoints(points, options) {
+    const { x, y, angle } = options;
+    const radians = fabric.util.degreesToRadians(angle);
+
+    return points.map(point => {
+      const translatedX = point.x;
+      const translatedY = point.y;
+      const rotatedX = translatedX * Math.cos(radians) - translatedY * Math.sin(radians);
+      const rotatedY = translatedX * Math.sin(radians) + translatedY * Math.cos(radians);
+      return { x: rotatedX + x, y: rotatedY + y };
+    });
+  }
+
+  updateVertices(){
+
+  }
+}
+
+function drawBasePolygon(points, options){
+  const baseGroup = new fabric.Group();
+  baseGroup.polygons = new GlyphPolygon(points, options)
+  baseGroup.addWithUpdate(baseGroup.polygons);
+
+  baseGroup.subObjects = []
+
+  baseGroup.polygons.vertex.forEach(v => {
+      // Draw a halftone circle 
+    const circle = new fabric.Circle({
+      left: v.x,
+      top: v.y,
+      radius: 15,
+      fill: 'rgba(180, 180, 180, 0.5)',
+      selectable: false,
+      originX: 'center',
+      originY: 'center',
+      opacity: 0,
+    });
+    baseGroup.subObjects.push(circle);
+
+    // Add a text label 
+    const text = new fabric.Text(v.label, {
+      left: v.x,
+      top: v.y - 30,
+      fontSize: 20,
+      fill: 'red',
+      selectable: false,
+      originX: 'center',
+      originY: 'center',
+      opacity: 0,
+    });
+    baseGroup.subObjects.push(text);
+    })
+  
+  baseGroup.subObjects.forEach(obj => {
+    baseGroup.addWithUpdate(obj);
+  })
+
+  baseGroup.on('selected', ()=>{
+    baseGroup.subObjects.forEach(obj => {
+      obj.set('opacity', 1)
+    })
+    canvas.renderAll(); 
+  });
+
+  baseGroup.on('deselected', ()=>{
+    baseGroup.subObjects.forEach(obj => {
+      obj.set('opacity', 0)
+    })
+    canvas.renderAll(); 
+  });
+
+  baseGroup.on('mouseover', function() { 
+    baseGroup.set({ 
+      opacity: 0.5
+    }); 
+    canvas.renderAll(); 
+  }); 
+  
+  baseGroup.on('mouseout', function() { 
+    baseGroup.set({ 
+      opacity: 1 }); 
+      canvas.renderAll(); 
+    });
+  
+  return baseGroup
+  
+}
+
 function drawLabeledArrow(canvas, options) {
   const { x, y, length, angle, color } = options;
 
@@ -208,7 +228,7 @@ function drawLabeledArrow(canvas, options) {
   ];
 
   // Create polygon with labeled vertices
-  const arrow = new GlyphPolygon(points, {
+  const arrow = new drawBasePolygon(points, {
     left: x,
     top: y,
     fill: color || 'black',
@@ -248,22 +268,21 @@ document.addEventListener('contextmenu', function (event) {
 
 document.getElementById('set-anchor').addEventListener('click', function () {
   if (selectedArrow) {
-    alert('Select a vertex from the arrow');
     // Implement vertex selection logic here
 
     // For simplicity, we'll use prompt for input
-    const vertexIndex1 = parseInt(prompt('Enter vertex index for Arrow 1 (e.g., 1 for V1):', '0'));
-    const vertexIndex2 = parseInt(prompt('Enter vertex index for Arrow 2 (e.g., 1 for V1):', '0'));
-    const spacingX = parseFloat(prompt('Enter spacing in X:', '0'));
+    const vertexIndex1 = parseInt(prompt('Enter vertex index for Arrow 1 (e.g., 1 for V1):', '1'));
+    const vertexIndex2 = parseInt(prompt('Enter vertex index for Arrow 2 (e.g., 1 for V1):', '1'));
+    const spacingX = parseFloat(prompt('Enter spacing in X:', '100'));
     const spacingY = parseFloat(prompt('Enter spacing in Y:', '0'));
 
     const arrow1 = selectedArrow;
     const arrow2 = canvasShapes.find(a => a !== arrow1);
 
-    const point1 = arrow1.points[vertexIndex1 - 1];
-    const point2 = arrow2.points[vertexIndex2 - 1];
+    const point1 = arrow1.polygons.points[vertexIndex1 - 1];
+    const point2 = arrow2.polygons.points[vertexIndex2 - 1];
 
-    const transformedPointsArrow2 = calculateTransformedPoints(arrow2.points, {
+    const transformedPointsArrow2 = calculateTransformedPoints(arrow2.polygons.points, {
       x: arrow2.left,
       y: arrow2.top,
       angle: arrow2.angle
@@ -277,7 +296,7 @@ document.getElementById('set-anchor').addEventListener('click', function () {
       top: targetPoint.y - point1.y + spacingY
     });
 
-    arrow1.updateVertices()
+    //arrow1.polygons.updateVertices()
     canvas.renderAll();
   }
 });
