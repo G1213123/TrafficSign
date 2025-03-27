2988d7ecee0b9379df8b34ddfdb49a9b03885949
Andrew Ng <ngwingfung2136@yahoo.com.hk>
2025-03-28 00:01:09 +0800

    0.9 Add tooltip functionality and styling to enhance user interface

--- js/sidebar/sb-border.js
+++ js/sidebar/sb-border.js
@@ -44,10 +44,16 @@ let FormBorderWrapComponent = {
       width: 161,
       height: 100
     };
+    const bboxExit = {
+      left: 0,
+      top: 0,
+      width: 60,
+      height: 100
+    };
 
     // Create SVG buttons for each border type
     Object.keys(BorderTypeScheme).forEach(async (borderType) => {
-      const shapeMeta = BorderTypeScheme[borderType](xHeight, bbox,);
+      const shapeMeta = BorderTypeScheme[borderType](xHeight, borderType=='exit'?bboxExit:bbox,);
       const svg = await FormBorderWrapComponent.createBorderSVG(shapeMeta,)
       GeneralHandler.createSVGButton(`button-${borderType}`, svg, parent, 'border', FormBorderWrapComponent.BorderCreateHandler, 'click')
     });
